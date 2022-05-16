11808436e46b3aef5a56b7bb78b6e75963bf603c
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-05-17 01:54:07 +0600

    jwt added

--- index.js
+++ index.js
@@ -64,7 +64,12 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
     });
 
     app.post("/bookings", async (req, res) => {
